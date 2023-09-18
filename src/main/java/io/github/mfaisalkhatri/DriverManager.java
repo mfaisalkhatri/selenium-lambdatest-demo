c152b699061d03a605dfb9b234d693d027ea5b32
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-09-18 18:59:28 +0530

    added config code to run tests on firefox on cloud

--- src/main/java/io/github/mfaisalkhatri/DriverManager.java
+++ src/main/java/io/github/mfaisalkhatri/DriverManager.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
@@ -16,13 +17,14 @@ import static java.text.MessageFormat.format;
 public class DriverManager {
     private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
     private static final String GRID_URL = "@hub.lambdatest.com/wd/hub";
-    private static final String LT_ACCESS_TOKEN = System.getProperty("LT_ACCESS_KEY");
+    private static final String LT_ACCESS_KEY = System.getProperty("LT_ACCESS_KEY");
     private static final String LT_USERNAME = System.getProperty("LT_USERNAME");
 
     public void createDriver(final Browsers browser) {
         switch (browser) {
             case FIREFOX -> setupFirefoxDriver();
             case CHROME_CLOUD -> setupChromeInLambdaTest();
+            case FIREFOX_CLOUD -> setupFirefoxInLambdaTest();
             default -> setupChromeDriver();
         }
         setupBrowserTimeouts();
@@ -73,12 +75,24 @@ public class DriverManager {
         browserOptions.setCapability("LT:Options", ltOptions());
         try {
             setDriver(
-                    new RemoteWebDriver(new URL(format("https://{0}:{1}{2}", LT_USERNAME, LT_ACCESS_TOKEN, GRID_URL)),
+                    new RemoteWebDriver(new URL(format("https://{0}:{1}{2}", LT_USERNAME, LT_ACCESS_KEY, GRID_URL)),
                             browserOptions));
         } catch (final MalformedURLException e) {
             throw new Error("Error setting up cloud browser in LambdaTest", e);
         }
+    }
 
+    private void setupFirefoxInLambdaTest() {
+        final var browserOptions = new FirefoxOptions();
+        browserOptions.setPlatformName("Windows 10");
+        browserOptions.setCapability("LT:Options", ltOptions());
+        try {
+            setDriver(
+                    new RemoteWebDriver(new URL(format("https://{0}:{1}{2}", LT_USERNAME, LT_ACCESS_KEY, GRID_URL)),
+                            browserOptions));
+        } catch (final MalformedURLException e) {
+            throw new Error("Error setting up cloud browser in LambdaTest", e);
+        }
     }
 
     private void setupFirefoxDriver() {
