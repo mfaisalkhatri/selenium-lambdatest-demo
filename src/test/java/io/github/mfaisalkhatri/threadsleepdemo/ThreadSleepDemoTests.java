20b4018a2c4b722d82ecf93f447a23c3b0ff73a8
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2024-04-19 01:11:19 +0530

    Added example code with lambdatest smartwait feature (#24)
    * added example code for smart wait
    
    * removed extra spaces from the test class

--- src/test/java/io/github/mfaisalkhatri/threadsleepdemo/ThreadSleepDemoTests.java
+++ src/test/java/io/github/mfaisalkhatri/threadsleepdemo/ThreadSleepDemoTests.java
@@ -1,10 +1,8 @@
 package io.github.mfaisalkhatri.threadsleepdemo;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -21,7 +19,7 @@ import static org.testng.Assert.assertEquals;
 
 public class ThreadSleepDemoTests {
 
-    private WebDriver driver;
+    private RemoteWebDriver driver;
 
     @BeforeTest
     public void setup() {
@@ -66,9 +64,6 @@ public class ThreadSleepDemoTests {
         this.driver.findElement(By.cssSelector(".entry-component .entry-widget nav.navbar ul li:nth-child(5) a")).click();
 
         Thread.sleep(1000);
-
-        final Actions actions = new Actions(driver);
-        actions.pause(2000);
     }
 
     @Test
@@ -82,6 +77,23 @@ public class ThreadSleepDemoTests {
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".entry-component .entry-widget nav.navbar ul li:nth-child(5) a"))).click();
     }
 
+    @Test
+    public void testLoginWithSmartWait() {
+        this.driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
+
+        final WebElement emailAddress = this.driver.findElement(By.id("input-email"));
+        emailAddress.sendKeys("david.thomson@gmail.com");
+
+        final WebElement password = this.driver.findElement(By.id("input-password"));
+        password.sendKeys("Secret@123");
+
+        final WebElement loginBtn = this.driver.findElement(By.cssSelector("input.btn-primary"));
+        loginBtn.click();
+
+        final String myAccountHeaderText = this.driver.findElement(By.cssSelector("#content h2")).getText();
+        assertEquals(myAccountHeaderText, "My Account");
+    }
+
     public ChromeOptions getChromeOptions() {
         final var browserOptions = new ChromeOptions();
         browserOptions.setPlatformName("Windows 10");
@@ -89,15 +101,14 @@ public class ThreadSleepDemoTests {
         final HashMap<String, Object> ltOptions = new HashMap<String, Object>();
         ltOptions.put("project", "Thread.sleep Demo on Cloud");
         ltOptions.put("build", "LambdaTest e-commerce website test");
-        ltOptions.put("name", "Thread.sleep demo test");
+        ltOptions.put("name", "Login test with SmartWait");
         ltOptions.put("w3c", true);
+        ltOptions.put("smartWait", 20);
         ltOptions.put("plugin", "java-testNG");
 
         browserOptions.setCapability("LT:Options", ltOptions);
 
         return browserOptions;
-
-
     }
 
     @AfterTest
