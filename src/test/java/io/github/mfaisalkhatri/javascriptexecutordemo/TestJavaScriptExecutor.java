ab28fe7d45c5848e1cac8fc2ad3c1809ec407f44
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-08-31 18:47:56 +0300

    added code for javascriptexecutor in selenium

--- src/test/java/io/github/mfaisalkhatri/javascriptexecutordemo/TestJavaScriptExecutor.java
+++ src/test/java/io/github/mfaisalkhatri/javascriptexecutordemo/TestJavaScriptExecutor.java
@@ -3,7 +3,6 @@ package io.github.mfaisalkhatri.javascriptexecutordemo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
@@ -32,8 +31,7 @@ public class TestJavaScriptExecutor {
         } catch (final MalformedURLException e) {
             System.out.println("Could not start the remote session on LambdaTest cloud grid");
         }
-        driver = new ChromeDriver();
-        this.driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        this.driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
     }
 
     @Test
@@ -41,10 +39,18 @@ public class TestJavaScriptExecutor {
         driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
 
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("document.getElementById('input-email').value='davidjacob@demo.com'");
-        js.executeScript("document.getElementById('input-password').value='Password123'");
+        WebElement emailAddressField = driver.findElement(By.id("input-email"));
+        js.executeScript("arguments[0].style.border='3px solid red'", emailAddressField);
+        emailAddressField.sendKeys("davidjacob@demo.com");
+        js.executeScript("arguments[0].style.border='2px solid #ced4da'", emailAddressField);
+
+        WebElement passwordField = driver.findElement(By.id("input-password"));
+        js.executeScript("arguments[0].style.border='3px solid red'", passwordField);
+        passwordField.sendKeys("Password123");
+        js.executeScript("arguments[0].style.border='2px solid #ced4da'", passwordField);
 
         WebElement loginBtn = driver.findElement(By.cssSelector("input.btn"));
+        js.executeScript("arguments[0].style.border='3px solid red'", loginBtn);
         js.executeScript("arguments[0].click();", loginBtn);
 
         String titleText = js.executeScript("return document.title;").toString();
@@ -54,8 +60,22 @@ public class TestJavaScriptExecutor {
         System.out.println("Domain is: " + domainName);
 
         String myAccountHeader = driver.findElement(By.cssSelector("#content h2")).getText();
-
         assertEquals(myAccountHeader, "My Account");
+        this.status = "passed";
+    }
+
+    @Test
+    public void testExecuteAsyncScript() {
+        driver.get("https://ecommerce-playground.lambdatest.io");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeAsyncScript("var callback = arguments[arguments.length - 1];" + "window.scrollBy(0,document.body.scrollHeight); + callback()");
+
+        String fromTheBlogText = driver.findElement(By.cssSelector("#entry_217991 > h3")).getText();
+        assertEquals(fromTheBlogText, "FROM THE BLOG");
+        this.status = "passed";
+
+        WebElement element = driver.findElement(By.id("shadowroot"));
+        js.executeScript("return arguments[0].shadowRoot", element);
     }
 
     public ChromeOptions getChromeOptions() {
@@ -72,15 +92,11 @@ public class TestJavaScriptExecutor {
         browserOptions.setCapability("LT:Options", ltOptions);
 
         return browserOptions;
-
     }
-
 
     @AfterTest
     public void tearDown() {
         this.driver.executeScript("lambda-status=" + this.status);
         this.driver.quit();
     }
-
-
 }
