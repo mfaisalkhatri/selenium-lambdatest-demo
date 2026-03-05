2ecf74790e58726c6934b2773a64b9e25c72b09d
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2026-03-05 16:02:07 +0530

    added image comparision test

--- src/test/java/io/github/mfaisalkhatri/visualtestopencv/CaptureScreenshot.java
+++ src/test/java/io/github/mfaisalkhatri/visualtestopencv/CaptureScreenshot.java
@@ -2,6 +2,7 @@ package io.github.mfaisalkhatri.visualtestopencv;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -11,10 +12,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CaptureScreenshot {
 
-    public static void main (final String[] args) throws IOException {
+    public static void captureActualImage () throws IOException {
         final WebDriver driver = new ChromeDriver ();
 
         driver.get ("https://ecommerce-playground.lambdatest.io/");
+        driver.manage ()
+            .timeouts ()
+            .implicitlyWait (Duration.ofSeconds (3));
 
         final File screenshot = ((TakesScreenshot) driver).getScreenshotAs (OutputType.FILE);
         FileUtils.copyFile (screenshot, new File ("screenshots/actual.png"));
